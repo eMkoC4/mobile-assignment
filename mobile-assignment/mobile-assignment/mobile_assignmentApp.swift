e35715e012d871ce05507bbd64eb3393fad5a4dc
Ivan Svoboda <ivansvoboda9@gmail.com>
2022-08-16 18:28:42 +0200

    Created the initial Xcode project

---
+++ mobile-assignment/mobile-assignment/mobile_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_assignmentApp.swift
+//  mobile-assignment
+//
+//  Created by IvanQ on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct mobile_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
